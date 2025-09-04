c3966a6789bd9536736a7fbf539ceefbd0bfb1b5
andrewrusso <and.russo.0511@gmail.com>
2025-09-04 22:34:12 +0200

    first commit

---
+++ FC.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void){
+printf("######\n");
+for (int i = 0; i < 2; i++)
+ {
+     printf("#\n");
+}
+printf("#####\n");
+for (int j = 0; j < 3; j++)
+{
+    printf("#\n");
+}
+
+printf("    ######\n");
+printf("  ##      ##\n");
+for (int k = 0; k < 5; k++)
+{
+    printf("#\n");
+}
+printf("  ##      ##\n");
+printf("    ######\n");
+}
---
+++ area_of_rect.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int h = 7;
+    int b = 5;
+
+    printf("Perimeter of the rectangle = %i\n", (h+b)*2);
+    printf("Perimeter of the rectangle = %i\n", h*b);
+}
